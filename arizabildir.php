ddeee482b1f8cc5a2e4acea4e4ac028b13693079
Yunus ARAS <yunus.kariha@gmail.com>
2015-03-12 16:54:03 +0200

    12.03.2015 - 16:53

---
+++ arizabildir.php
@@ -0,0 +1,24 @@
+<?php include 'inc/head.html';?>
+<body>
+    <?php include 'inc/banner-menu-kullanici.html'; ?>
+
+    <div class="kapsul">
+        <h1 class="h1Tag">Formu doldurarak arızanızı bildiriniz, kısa sürede çözüme kavuşturulacaktır.</h1>        
+        <hr class="cetvel"/>
+        <form id="formUyelik" class="uyelikFormu" action="#" method="POST">
+            <label for="arizaTuru" class="uyelikLabel">Arızanız için bir kategori seçiniz : </label>
+            <select class="selectBox" name="arizaTuru" id="arizaTuru">
+                <option value="ariza-1" selected>ariza-1</option>
+                <option value="ariza-2">ariza-2</option>
+                <option value="ariza-3">ariza-3</option>
+                <option value="ariza-4">ariza-4</option>
+                <option value="ariza-5">ariza-5</option>
+            </select><br />
+            <label class="textareaLabel" for="hakkinda">Arızanız hakkında açıklama giriniz : </label>
+            <textarea name="arizaBilgisi" id="arizaBilgisi"></textarea>
+            <input class="button" type="submit" value="Arızayı bildir." />
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ duyurular.php
@@ -0,0 +1,15 @@
+<?php include 'inc/head.html';?>
+<body>
+    <?php include 'inc/banner-menu-kullanici.html'; ?>
+    
+    <div class="kapsul">
+        <h1 class="h1Tag">Öğrenci evleri güncel duyuruları.</h1>
+        <hr class="cetvel" />
+        <span class="duyuru acil">Öğrenci evlerinde yangın var bütün blokları boşaltın.</span>
+        <span class="duyuru bilgi">Öğrenci evleri A,B,C,F-J ve T Bloklarında elektrik kesintisi yaşanacak.<br /> Sular da kesilirse şaşırmayın!!!!!</span>
+        <span class="duyuru soru">Aydatlarını yatırmayanlar kimler?</span>
+        <span class="duyuru normal">Öğrenci evleri önünde kaymak yasaktır.</span>
+    </div>
+
+</body>
+</html>
