9ee46e600010ab41c252ce86b3b467fa9b72593c
Yunus ARAS <yunus.kariha@gmail.com>
2015-03-12 18:30:38 +0200

    Linkler yenilendi

--- duyurular.php
+++ duyurular.php
@@ -5,10 +5,10 @@
     <div class="kapsul">
         <h1 class="h1Tag">Öğrenci evleri güncel duyuruları.</h1>
         <hr class="cetvel" />
-        <span class="duyuru acil">Öğrenci evlerinde yangın var bütün blokları boşaltın.</span>
-        <span class="duyuru bilgi">Öğrenci evleri A,B,C,F-J ve T Bloklarında elektrik kesintisi yaşanacak.<br> Sular da kesilirse şaşırmayın!!!!!</span>
-        <span class="duyuru soru">Aydatlarını yatırmayanlar kimler?</span>
-        <span class="duyuru normal">Öğrenci evleri önünde sigara içmek yasaktır.</span>
+        <span class="duyuru acil">Öğrenci evlerinde yangın var bütün blokları boşaltın.<span class="duyuruTarihi">12.03.2015</span></span>
+        <span class="duyuru bilgi">Öğrenci evleri A,B,C,F-J ve T Bloklarında elektrik kesintisi yaşanacak.<br> Sular da kesilirse şaşırmayın!!!!!<span class="duyuruTarihi">12.03.2015</span></span>
+        <span class="duyuru soru">Aydatlarını yatırmayanlar kimler?<span class="duyuruTarihi">12.03.2015</span></span>
+        <span class="duyuru normal">Öğrenci evleri önünde sigara içmek yasaktır.<span class="duyuruTarihi">12.03.2015</span></span>
     </div>
 
 </body>
