36f24bf2b5c36de63af737dbf06ce7ecd9a1a520
Yunus ARAS <yunus.kariha@gmail.com>
2015-03-25 16:06:13 +0200

    Profil sayfası yapıldı

--- profil.php
+++ profil.php
@@ -4,18 +4,19 @@
 
     <div class="kapsul">
         <div class="fotoKapsul">
-            <img class="profilFoto" src="images/profil.png" alt="Profil Fotoğrafı"/>
+            <img class="profilFoto" src="<?php echo $_SESSION['userData']['fotograf']; ?>" alt="Profil Fotoğrafı"/>
         </div>
         <div class="profilBilgileri">
             <h1 class="h1Tag">Profil Bilgileriniz.</h1>
             <hr class="cetvel" />
             <form class="formKullaniciBilgileri" action="#" method="POST" id="formOgrBilgiler">
-                <label for="adSoyad">Adınız ve Soyadınız : </label><input type="text" name="adSoyad" id="adSoyad" required placeholder="Mehmet Ahmet" required />
-                <label for="ogrNo">Öğrenci Numaranız   : </label><input type="text" name="ogrNo" id="ogrNo" required placeholder="21143333" disabled="disabled" required />
-                <label for="telefon">Telefon Numaranız : </label><input type="text" name="telefon" id="telefon" required placeholder="05343443434" disabled="disabled" required />
-                <label for="eposta">Elektronik Posta Adresiniz: </label><input type="email" name="eposta" id="eposta" required placeholder="at@at.com" disabled="disabled" required />
+                <label for="adSoyad">Adınız ve Soyadınız : </label><input type="text" name="adSoyad" id="adSoyad" value="<?php echo $_SESSION['userData']['adsoyad']; ?>" required />
+                <label for="ogrNo">Öğrenci Numaranız   : </label><input type="text" name="ogrNo" id="ogrNo" disabled="disabled" value="<?php echo $_SESSION['userData']['numara']; ?>" required />
+                <label for="telefon">Telefon Numaranız : </label><input type="text" name="telefon" id="telefon" disabled="disabled" value="<?php echo $_SESSION['userData']['telefon']; ?>" required />
+                <label for="eposta">Elektronik Posta Adresiniz: </label><input type="email" name="eposta" id="eposta" disabled="disabled" value="<?php echo $_SESSION['userData']['eposta']; ?>" required />
                 <label>Blok - Kat ve Oda Numaranız : </label><select name="blok" id="blok"  disabled="disabled">
-                    <option value="A" selected>A Blok</option>
+                    <option value="<?php echo $_SESSION['userData']['blok']; ?>" selected><?php echo $_SESSION['userData']['blok']; ?></option>
+                    <option value="B">A Blok</option>
                     <option value="B">B Blok</option>
                     <option value="C">C Blok</option>
                     <option value="D">D Blok</option>
@@ -31,12 +32,14 @@
                     <option value="N">N Blok</option>
                 </select>
                 <select name="kat" id="kat"  disabled="disabled">
-                    <option value="0" selected>Zemin</option>
+                    <option value="<?php echo $_SESSION['userData']['kat']; ?>" selected><?php echo $_SESSION['userData']['kat']; ?></option>
+                    <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                 </select>
                 <select name="oda" id="oda"  disabled="disabled">
+                    <option value="<?php echo $_SESSION['userData']['oda']; ?>" selected><?php echo $_SESSION['userData']['oda']; ?></option>
                     <option value="01" selected>01</option>
                     <option value="02">02</option>
                     <option value="03">03</option>
