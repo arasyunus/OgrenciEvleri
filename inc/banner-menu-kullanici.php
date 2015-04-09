1724c1867ae907985ca029688a330fb74fbcdb33
Yunus ARAS <yunus.kariha@gmail.com>
2015-04-09 16:50:15 +0300

    Style repeat-x ve cuma teslimi icin hazirlandi

--- inc/banner-menu-kullanici.php
+++ inc/banner-menu-kullanici.php
@@ -20,7 +20,7 @@
 </div>
 <div class="banner">
 
-    <a href="#"><div class="hupng"></div></a>
+    <a href="index.php"><div class="hupng"></div></a>
 
     <div class="bannerMenu">
         <ul class="banMenu">
